cdbfd7687c96f1c318d933f44148ad6f0c3d1436
Hammad39 <hammadimran3@gmail.com>
2022-07-21 15:16:00 +0500

    NextAuth Implementation

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,7 +15,9 @@ import {
   VideoCameraIcon,
 } from "@heroicons/react/outline";
 import Image from "next/image";
+import { signIn, signOut, useSession } from "next-auth/react";
 export default function <Image>() {
+  const {data : session} = useSession();
   return (
     <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
       <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
@@ -54,17 +56,32 @@ export default function <Image>() {
         <MenuIcon className="icon" />
       </div>
       {/* Sign In / Sign out button */}
-      <div className="hidden lg:flex items-center space-x-2 border border-gray-200 p-2">
-        <div className="relative h-5 w-5 flex-shrink-0">
-          <Image
-            src="https://links.papareact.com/23l"
-            objectFit="contain"
-            layout="fill"
-            alt=""
-          />
-        </div>
-        <p className="text-gray-400">Sign in </p>
+      {session? 
+      (      <div onClick={()=>signOut()}  className="hover:cursor-pointer hidden lg:flex items-center space-x-2 border border-gray-200 p-2">
+      <div className="relative h-5 w-5 flex-shrink-0">
+        <Image
+          src="https://links.papareact.com/23l"
+          objectFit="contain"
+          layout="fill"
+          alt=""
+        />
       </div>
+      <p className="text-gray-400">Sign out </p>
+    </div>)
+      : 
+      (      <div onClick={()=>signIn()}  className=" hover:cursor-pointer hidden lg:flex items-center space-x-2 border border-gray-200 p-2">
+      <div className="relative h-5 w-5 flex-shrink-0">
+        <Image
+          src="https://links.papareact.com/23l"
+          objectFit="contain"
+          layout="fill"
+          alt=""
+        />
+      </div>
+      <p className="text-gray-400">Sign in </p>
+    </div>)
+      }
+
     </div>
   );
 }
